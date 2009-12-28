85ee625e1d132129829131e8f12a91569e9e8db8
arnimarj@gmail.com <arnimarj@gmail.com@02f57f8a-f267-11de-9907-777483041811>
2009-12-28 00:08:54 +0000

    Changed C API test harness: now, not everything is in a single file

---
+++ test/c_api/test.h
@@ -0,0 +1,39 @@
+#ifndef __POINTLESS__TEST__H__
+#define __POINTLESS__TEST__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include <pointless/pointless.h>
+#include <pointless/pointless_recreate.h>
+
+#define CHECK_HANDLE(handle) if (handle == POINTLESS_CREATE_VALUE_FAIL) { fprintf(stderr, #handle " creation failure"); exit(EXIT_FAILURE); }
+
+// hash validator
+void validate_hash_semantics();
+
+// create/query test-cases
+typedef void (*create_cb)(pointless_create_t* c);
+typedef void (*query_cb)(pointless_t* p);
+
+void create_wrapper(const char* fname, create_cb cb);
+void query_wrapper(const char* fname, query_cb cb);
+
+void create_tuple(pointless_create_t* c);
+void create_very_simple(pointless_create_t* c);
+void create_simple(pointless_create_t* c);
+void create_set(pointless_create_t* c);
+void query_set(pointless_t* p);
+void create_special_a(pointless_create_t* c);
+void create_special_b(pointless_create_t* c);
+void create_special_c(pointless_create_t* c);
+static uint32_t create_special_d_0(pointless_create_t* c);
+static uint32_t create_special_d_1(pointless_create_t* c, uint32_t* buffer);
+static uint32_t create_special_d_2(pointless_create_t* c);
+static uint32_t create_special_d_3(pointless_create_t* c);
+void create_special_d(pointless_create_t* c);
+void query_special_d(pointless_t* p);
+
+#endif
---
+++ test/c_api/validate_hash.c
@@ -0,0 +1,53 @@
+#include "test.h"
+
+void validate_hash_semantics()
+{
+	uint64_t i = 0;
+	int32_t v = INT32_MIN;
+	uint32_t h_i32, h_u32, h_f;
+
+	pointless_value_t i32, u32, f;
+
+	union {
+		int32_t i32;
+		uint32_t u32;
+		float f;
+	} hash;
+
+	while (i <= 0xFFFFFFFF) {
+		i += 1;
+
+		i32.data.data_i32 = v;
+		u32.data.data_i32 = v;
+		f.data.data_f = (float)v;
+
+		hash.i32 = v;
+		h_i32 = pointless_hash_i32(hash.i32);
+
+		hash.i32 = v;
+		h_u32 = pointless_hash_u32(hash.u32);
+
+		hash.f = (float)v;
+		h_f = pointless_hash_float(hash.f);
+
+		if (h_i32 != h_u32) {
+			fprintf(stderr, "v: %i i32/u32 hash mismatch: %u/%u\n", v, h_i32, h_u32);
+			exit(EXIT_FAILURE);
+		}
+
+		if (h_u32 != h_f) {
+			fprintf(stderr, "v: %i u32/f hash mismatch: %u/%u\n", v, h_u32, h_f);
+			exit(EXIT_FAILURE);
+		}
+
+		if (h_i32 != h_f) {
+			fprintf(stderr, "v: %i i32/f hash mismatch: %u/%u\n", v, h_i32, h_f);
+			exit(EXIT_FAILURE);
+		}
+
+		if (i % (1024*1024*16) == 0) {
+			printf("0x%08x\n", (uint32_t)i);
+			fflush(stdout);
+		}
+	}
+}
