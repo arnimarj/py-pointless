e6dded2072a24f675db2da27b554a332c63f978f
arnimarj@gmail.com <arnimarj@gmail.com@02f57f8a-f267-11de-9907-777483041811>
2011-05-23 15:58:26 +0000

    Added some code which does (slow) unsigned integer multiplication and addition with overflow checking. GCC only, I think.

---
+++ include/pointless/pointless_int_ops.h
@@ -0,0 +1,11 @@
+#ifndef __POINTLESS__INT__OPS__H__
+#define __POINTLESS__INT__OPS__H__
+
+#include <stdlib.h>
+#include <stdint.h>
+
+int sizet_mult(size_t a, size_t b, size_t* out);
+int u64_mult(uint64_t a, uint64_t b, uint64_t* out);
+int u32_mult(uint32_t a, uint32_t b, uint32_t* out);
+
+#endif
---
+++ src/pointless_int_ops.c
@@ -0,0 +1,28 @@
+#include <pointless/pointless_int_ops.h>
+
+typedef unsigned int uint128_t __attribute__((mode(TI)));
+
+#define __OVERFLOW_MULT(a, b, out, T, M) {uint128_t v = (uint128_t)(a) * (uint128_t)(b); *(out) = (T)v; return (v <= M); }
+#define __OVERFLOW_ADD_2(a, b, out, T, M) {uint128_t v = (uint128_t)(a) + (uint128_t)(b); *(out) = (T)v; return (v <= M); }
+#define __OVERFLOW_ADD_3(a, b, c, out, T, M) {uint128_t v = (uint128_t)(a) + (uint128_t)(b) + (uint128_t)(c); *(out) = (T)v; return (v <= M); }
+
+int sizet_mult(size_t a, size_t b, size_t* out)
+{
+	__OVERFLOW_MULT(a, b, out, size_t, SIZE_MAX);
+}
+
+int u64_mult(uint64_t a, uint64_t b, uint64_t* out)
+{
+	__OVERFLOW_MULT(a, b, out, uint64_t, UINT64_MAX);
+}
+
+int u32_mult(uint32_t a, uint32_t b, uint32_t* out)
+{
+	__OVERFLOW_MULT(a, b, out, uint32_t, UINT32_MAX);
+}
+
+#undef __OVERFLOW_MULT
+#undef __OVERFLOW_ADD_2
+#undef __OVERFLOW_ADD_3
+
+#undef uint128_t
