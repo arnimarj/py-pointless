f6f6e01848c250efaeb2e8d7671a40b00b63f9ec
arni <arnimarj@gmail.com>
2014-10-22 15:05:38 +0000

    bumping version number

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extra_link_args = ['-L./judy-1.0.5/src', '-Bstatic', '-lJudy', '-Bdynamic', '-lm
 
 setup(
 	name = 'pointless',
-	version = '0.2.3',
+	version = '0.2.4',
 	maintainer = 'Arni Mar Jonsson',
 	maintainer_email = 'arnimarj@gmail.com',
 	url = 'http://code.google.com/p/py-pointless/',
