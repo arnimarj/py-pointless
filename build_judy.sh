d3fac61280281dbd3f72e0a69abae678d24f347e
Ubuntu <johndoe@example.co>
2016-06-13 15:45:34 +0000

    allowing CC/CXX variables to be propogate to sh_build

--- build_judy.sh
+++ build_judy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-(cd judy-1.0.5/src; COPT='-DJU_64BIT -O0 -fPIC -fno-strict-aliasing -fno-aggressive-loop-optimizations' sh ./sh_build)
+if [[ -v CXX ]]; then
+	JUDYCC=$CXX
+elif [[ -v CC ]]; then
+	JUDYCC=$CC
+else
+	JUDYCC=CC
+fi
+
+echo CC $JUDYCC
+
+(cd judy-1.0.5/src; CC=$JUDYCC COPT='-DJU_64BIT -O0 -fPIC -fno-strict-aliasing -fno-aggressive-loop-optimizations' sh ./sh_build)
