ee83e3de5dfeea5ada7199e0263b04f8a60f46c6
Ubuntu <johndoe@example.co>
2016-06-13 16:00:07 +0000

    no gxx

--- build_judy.sh
+++ build_judy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [[ -v CXX ]]; then
-	JUDYCC=$CXX
-elif [[ -v CC ]]; then
+if [[ -v CC ]]; then
 	JUDYCC=$CC
 else
-	JUDYCC=CC
+	JUDYCC=cc
 fi
-
-echo CC $JUDYCC
 
 (cd judy-1.0.5/src; CC=$JUDYCC COPT='-DJU_64BIT -O0 -fPIC -fno-strict-aliasing -fno-aggressive-loop-optimizations' sh ./sh_build)
