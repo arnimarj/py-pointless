3ff3c20a11bc6bda3c7e60d4de19d32eb413832d
Kristjan G. Bjarnason <kgb@dohop.com>
2016-10-29 11:31:51 +0000

    fixing judy build script on macos

--- build_judy.sh
+++ build_judy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [[ -v CC ]]; then
+if [ -z CC ]; then
 	JUDYCC=$CC
 else
 	JUDYCC=cc
 fi
 
-(cd judy-1.0.5/src; CC=$JUDYCC COPT='-DJU_64BIT -O0 -fPIC -fno-strict-aliasing -fno-aggressive-loop-optimizations' sh ./sh_build)
+COPT="-DJU_64BIT -O0 -fPIC -fno-strict-aliasing"
+
+if $JUDYCC --version | grep clang >& /dev/null; then
+	COPT+=""
+else
+	COPT+=" -fno-aggressive-loop-optimizations"
+fi
+
+(cd judy-1.0.5/src; CC=$JUDYCC COPT="$COPT" sh ./sh_build)
