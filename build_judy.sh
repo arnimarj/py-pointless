79fe1de4a21faee2ed00b83178de7ab985076d2f
arnimarj <arnimarj@gmail.com>
2018-06-08 14:42:53 +0000

    update build script for judyt

--- build_judy.sh
+++ build_judy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+set -e
+
+basename="$(dirname $0)"
+
 if [ -z CC ]; then
 	JUDYCC=$CC
 else
 	JUDYCC=cc
 fi
 
-COPT="-DJU_64BIT -O0 -fPIC -fno-strict-aliasing"
+if [ ! -z COPT ]; then
+	COPT="-DJU_64BIT -O0 -fPIC -fno-strict-aliasing"
+fi
+
 
 if $JUDYCC -v 2>&1 >/dev/null | egrep "clang|gcc version 4.6"; then
 	COPT+=""
@@ -14,4 +21,4 @@ else
 	COPT+=" -fno-aggressive-loop-optimizations"
 fi
 
-(cd judy-1.0.5/src; CC=$JUDYCC COPT="$COPT" sh ./sh_build)
+(cd $basename; cd judy-1.0.5/src; CC=$JUDYCC COPT="$COPT" sh ./sh_build)
