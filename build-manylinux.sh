905cfbd4832a772afc45ec4d09de973aec7f840d
arnimarj <arnimarj@gmail.com>
2019-12-05 15:06:57 +0000

    wip

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p ./wheelhouse
 
 docker run --rm -e PLAT=manylinux2014_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2014_x86_64    /io/travis/build-wheels.sh
--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,24 +5,26 @@ is_mypy=$?
 
 set -ex
 
+mkdir -p ./wheels
+
 for PYBIN in /opt/python/*/bin; do
 	# pypy, but only pypy3
 	if [ $is_mypy -eq 0 ]; then
 		if [[ $PYBIN == *"pypy3"* ]]; then
 
-			"${PYBIN}/pip" wheel /io/ -w wheelhouse/
+			"${PYBIN}/pip" wheel /io/ -w wheels/
 		fi
 	# cpython
 	else
 		if [[ $PYBIN =~ cp35|cp36|cp37|cp38 ]]; then
 
-			"${PYBIN}/pip" wheel /io/ -w wheelhouse/
+			"${PYBIN}/pip" wheel /io/ -w wheels/
 		fi
 	fi
 done
 
-for whl in wheelhouse/*.whl; do
-	auditwheel repair "$whl" --plat "$PLAT"
+for whl in wheels/*.whl; do
+	auditwheel repair "$whl" --plat "$PLAT" -w ./wheelhouse/
 done
 
 cp wheelhouse/*.whl /io/wheelhouse/
