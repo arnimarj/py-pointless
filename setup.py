3bb9319d8ceaabdbdb591c5f201436d7ec12bd90
Árni Már Jónsson <arnimarj@gmail.com>
2020-01-10 15:17:14 +0000

    manylinux2014 (#18)

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ extra_link_args = ['-L./judy-1.0.5/src', '-Bstatic', '-lJudy', '-Bdynamic', '-lm
 
 setup(
 	name='pointless',
-	version='1.0.1',
+	version='1.0.2',
 	maintainer='Arni Mar Jonsson',
 	maintainer_email='arnimarj@gmail.com',
 	url='https://github.com/arnimarj/py-pointless',
