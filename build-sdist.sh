abaeb5a68517a84d74a05a0d2c71cd41937672a7
arnimarj <arnimarj@gmail.com>
2020-01-21 21:37:27 +0000

    building sdist

---
+++ build-sdist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p ./wheelhouse
+
+python3 setup.py build sdist --dist-dir ./wheelhouse
+
+# test it
+pip3 install -U pip virtualenv
+python -m virtualenv venv
+./venv/bin/pip install ./wheelhouse/*.gz
+
+
+ls -l ./wheelhouse
