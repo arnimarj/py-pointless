cae17c98ed765719377478af891fa2ca49e1d307
arnimarj <arnimarj@gmail.com>
2020-01-21 21:37:38 +0000

    building sdist

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ extra_link_args = ['-L./judy-1.0.5/src', '-Bstatic', '-lJudy', '-Bdynamic', '-lm
 
 setup(
 	name='pointless',
-	version='1.0.2',
+	version='1.0.3',
 	maintainer='Arni Mar Jonsson',
 	maintainer_email='arnimarj@gmail.com',
 	url='https://github.com/arnimarj/py-pointless',
