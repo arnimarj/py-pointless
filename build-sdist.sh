2dff9404dd20d6cf82d66f88cc1b9e8edca2af78
arnimarj <arnimarj@gmail.com>
2025-11-20 23:09:26 +0000

    lets try this

--- build-sdist.sh
+++ build-sdist.sh
@@ -4,9 +4,13 @@ set -xe
 
 mkdir -p ./wheelhouse
 
-python setup.py sdist --dist-dir ./wheelhouse
+./build_judy.sh
+
+pip install pipx
+pipx run build --sdist --outdir ./wheelhouse .
 
 # test it
 pip install -U pip virtualenv
 python -m virtualenv venv
+
 ./venv/bin/pip install ./wheelhouse/*.gz
